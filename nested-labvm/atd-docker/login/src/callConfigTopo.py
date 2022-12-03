4e426e58e2554ca3ebac087b525e2fd645719ea2
tatanithin <113345721+tatanithin@users.noreply.github.com>
2022-12-03 05:26:22 +0000

    fixed arg values

--- nested-labvm/atd-docker/login/src/callConfigTopo.py
+++ nested-labvm/atd-docker/login/src/callConfigTopo.py
@@ -1,4 +1,4 @@
 from ConfigureTopology.ConfigureTopology import ConfigureTopology
 import sys
 #ConfigureTopology(selected_menu='training-l3',selected_lab='reset',public_module_flag=True)
-ConfigureTopology(selected_menu=str(sys.argv[0]),selected_lab=str(sys.argv[1]),public_module_flag=True)
+ConfigureTopology(selected_menu=str(sys.argv[1]),selected_lab=str(sys.argv[2]),public_module_flag=True)
