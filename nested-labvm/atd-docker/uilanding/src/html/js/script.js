ac289c5b219152895763d066db786a38027563bb
tatanithin <113345721+tatanithin@users.noreply.github.com>
2024-04-25 12:43:04 -0300

    Update script.js

--- nested-labvm/atd-docker/uilanding/src/html/js/script.js
+++ nested-labvm/atd-docker/uilanding/src/html/js/script.js
@@ -1,3 +1,4 @@
+labStatusInterval;
 $(document).ready(function () {
   $(".menu-icon,.menu-click").click(function () {
     $(".left-sidebar").toggleClass("active");
@@ -7,9 +8,13 @@ $(document).ready(function () {
     $("#main").show();
   });
   $(".menu-click").click(function () {
+    var elements = document.getElementsByClassName('menu-click');
 
+    for (var i = 0; i < elements.length; i++) {
+        elements[i].classList.remove('current-page');
+    }
     $id = $(this).data("id");
-    $id.addClass("current-page");
+    $(this).addClass("current-page");
     if ($id == "lab-status" || $id == "lab-menu") {
       $(".panel").hide();
       
@@ -31,3 +36,22 @@ $(document).ready(function () {
   });
   $(document).foundation();
 });
+
+
+
+$(function() {
+    $('.lab-button').click(function() {
+      // Remove 'active' class from all buttons
+      $('.lab-button').removeClass('active');
+  
+      // Add 'active' class to the clicked button
+      $(this).addClass('active');
+  
+      // Print the button's text to the console
+      console.log($(this).text().trim());
+  
+      // Prevent default action if it's an anchor tag
+      return false;
+    });
+  });
+  
