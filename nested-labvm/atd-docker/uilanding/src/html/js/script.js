64d681b63804c97d45bd5b42d1b75c2506056a4f
tatanithin <113345721+tatanithin@users.noreply.github.com>
2024-04-28 13:07:54 -0400

    Update script.js

--- nested-labvm/atd-docker/uilanding/src/html/js/script.js
+++ nested-labvm/atd-docker/uilanding/src/html/js/script.js
@@ -1,4 +1,43 @@
-labStatusInterval;
+
+labStatusInterval = null
+
+$(document).ready(function () {
+    $(".menu-icon,.menu-click").click(function () {
+      $(".left-sidebar").toggleClass("active");
+    });
+    $(".topology").click(function () {
+      $("#dashboard").hide();
+      $("#main").show();
+    });
+    $(".menu-click").click(function () {
+        console.log("menu-click")
+      $id = $(this).data("id");
+      $(this).addClass("current-page");
+      if ($id == "lab-status" || $id == "lab-menu") {
+        $(".panel").hide();
+        
+      } else {
+        $(".panel").show();
+      }
+      if ($id == "lab-status") {
+        getLabStatus()
+        labStatusInterval = setInterval(
+          () => {
+            getLabStatus()
+          }, 30000
+        )
+      } else {
+        clearInterval(labStatusInterval)
+      }
+      $("#" + $id).show();
+      
+    });
+    $(document).foundation();
+  });
+
+
+
+
 $(document).ready(function () {
   $(".menu-icon,.menu-click").click(function () {
     $(".left-sidebar").toggleClass("active");
