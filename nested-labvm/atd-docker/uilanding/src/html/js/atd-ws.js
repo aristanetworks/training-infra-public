beab10a07e77305ebd2a1aff1eeab00b99c24d36
tatanithin <113345721+tatanithin@users.noreply.github.com>
2025-02-21 13:29:27 -0500

    Update atd-ws.js

--- nested-labvm/atd-docker/uilanding/src/html/js/atd-ws.js
+++ nested-labvm/atd-docker/uilanding/src/html/js/atd-ws.js
@@ -79,7 +79,7 @@ function createWS(SOCK_URL) {
             var reg_data = received_msg['data'];
             if ('uptime' in reg_data) {
                 uptime_data = reg_data['uptime'];
-                examInstanceCountdown('countdown_timer', reg_data[endexamtime]);
+                examInstanceCountdown('countdown_timer', reg_data['endexamtime']);
             }
             if ('cvp' in reg_data) {
                 _cvp_info = "<h3>CVP " + reg_data['cvp']['version'] + " is currently " + reg_data['cvp']['status'] + "</h3>";
