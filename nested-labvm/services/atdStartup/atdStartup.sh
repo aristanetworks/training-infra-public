fc4ffe389c36ba2668472c91815e36efcd4c7d05
tatanithin <113345721+tatanithin@users.noreply.github.com>
2025-06-17 12:21:43 -0300

    Update atdStartup.sh

--- nested-labvm/services/atdStartup/atdStartup.sh
+++ nested-labvm/services/atdStartup/atdStartup.sh
@@ -213,3 +213,5 @@ rsync -av /opt/atd/nested-labvm/services/atdStartup/exam-submission-check.timer 
 sudo systemctl daemon-reload
 sudo systemctl enable exam-submission-check.timer
 sudo systemctl start exam-submission-check.timer
+# Enable password authentication for SSH
+sudo sed -i 's/^PasswordAuthentication no/PasswordAuthentication yes/' /etc/ssh/sshd_config && sudo systemctl restart sshd
