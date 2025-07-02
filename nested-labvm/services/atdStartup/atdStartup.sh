8d9aaf2b174e224009b913ce107e8ec2582f57a5
rparkinarista <44816118+rparkinarista@users.noreply.github.com>
2025-07-02 11:08:08 +0100

    Update atdStartup.sh
    add changes for SSH config back

--- nested-labvm/services/atdStartup/atdStartup.sh
+++ nested-labvm/services/atdStartup/atdStartup.sh
@@ -213,5 +213,5 @@ rsync -av /opt/atd/nested-labvm/services/atdStartup/exam-submission-check.timer 
 sudo systemctl daemon-reload
 sudo systemctl enable exam-submission-check.timer
 sudo systemctl start exam-submission-check.timer
-# echo "Password authentication enabled for SSH"
-# sudo sed -i 's/^PasswordAuthentication no/PasswordAuthentication yes/' /etc/ssh/sshd_config && sudo systemctl restart sshd
+echo "Password authentication enabled for SSH"
+sudo sed -i 's/^PasswordAuthentication no/PasswordAuthentication yes/' /etc/ssh/sshd_config && sudo systemctl restart sshd
