f462024e0cad0128c5f2101d7947bd1fdf3a1fda
Rich Parkin <rparkin@arista.com>
2025-12-18 18:34:31 +0000

    mode fixes

--- nested-labvm/atd-docker/uilanding/src/html/js/backend.js
+++ nested-labvm/atd-docker/uilanding/src/html/js/backend.js
@@ -348,19 +348,27 @@ var btn = document.getElementById("myBtn");
 // Get the <span> element that closes the modal
 var span = document.getElementsByClassName("close")[0];
 
-// When the user clicks the button, open the modal 
-btn.onclick = function () {
-    modal.style.display = "block";
+// When the user clicks the button, open the modal
+// Only attach if both btn and modal exist
+if (btn && modal) {
+    btn.onclick = function () {
+        modal.style.display = "block";
+    }
 }
 
 // When the user clicks on <span> (x), close the modal
-span.onclick = function () {
-    modal.style.display = "none";
+// Only attach if both span and modal exist
+if (span && modal) {
+    span.onclick = function () {
+        modal.style.display = "none";
+    }
 }
 
 // When the user clicks anywhere outside of the modal, close it
-window.onclick = function (event) {
-    if (event.target == modal) {
-        modal.style.display = "none";
+if (modal) {
+    window.onclick = function (event) {
+        if (event.target == modal) {
+            modal.style.display = "none";
+        }
     }
 }    
