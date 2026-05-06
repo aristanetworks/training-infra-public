9feb0568f786c21f300aeb7d659ee369c4f76372
tatanithin <113345721+tatanithin@users.noreply.github.com>
2026-05-06 12:31:39 -0400

    Update pages.py

--- nested-labvm/atd-docker/uilanding/src/handlers/pages.py
+++ nested-labvm/atd-docker/uilanding/src/handlers/pages.py
@@ -126,6 +126,10 @@ class topoRequestHandler(BaseHandler):
                         gui_urls.append(f'http://{response.text}:{servers[server]["port"]}')
                 except Exception as e:
                     safe_log('error', f'Error in topoRequestHandler: {e}', event='error', handler='topoRequestHandler')
+            try:
+                student_name = host_yaml.get('customer_details', {}).get('exam_taker_full_name', '').strip()
+            except Exception:
+                student_name = ''
             self.render(
                 self.base_path + 'index.html',
                 NODES=self.mod_yaml['topology']['nodes'],
@@ -137,7 +141,8 @@ class topoRequestHandler(BaseHandler):
                 labguides=labguides,
                 topo_cvp=_topo_cvp,
                 menu_options=menu,
-                lab_type=lab_type
+                lab_type=lab_type,
+                student_name=student_name
             )
 
 
